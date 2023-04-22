55ab2c66fbccc7e034a4a0fb16ea570bb8b1fcd1
D-Toshchakov <daniiltoshchakov@gmail.com>
2023-04-22 15:05:25 +0300

    created dockerfile for email service and added it to compose

--- apps/email/src/email.module.ts
+++ apps/email/src/email.module.ts
@@ -3,10 +3,16 @@ import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
 import { ConfigModule } from '@nestjs/config';
 import { RmqModule } from 'libs/common';
+import * as Joi from 'joi';
 
 @Module({
   imports: [ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        RABBIT_MQ_URI: Joi.string().required(),
+        RABBIT_MQ_TELEGRAM_QUEUE: Joi.string().required(),
+        PORT: Joi.number().required()
+      }),
       envFilePath: './apps/email/.env'
     }),
     RmqModule,
--- apps/email/src/main.ts
+++ apps/email/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { EmailModule } from './email.module';
 import { RmqService } from 'libs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(EmailModule);
   const rmqService = app.get<RmqService>(RmqService);
   app.connectMicroservice(rmqService.getOptions('TELEGRAM'));
+  
+  const configService = app.get(ConfigService)
+  const PORT = configService.get('PORT')
+  await app.listen(PORT)
   await app.startAllMicroservices()
 }
 bootstrap();
