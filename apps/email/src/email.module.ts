b435aeeb20ee79b60453b6e915501eb522f1f1d1
D-Toshchakov <daniiltoshchakov@gmail.com>
2023-04-22 13:42:40 +0300

    added rmq service and docker-compose

--- apps/email/src/email.module.ts
+++ apps/email/src/email.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { EmailController } from './email.controller';
 import { EmailService } from './email.service';
+import { ConfigModule } from '@nestjs/config';
+import { RmqModule } from 'libs/common';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: './apps/email/.env'
+    }),
+    RmqModule,
+  ],
   controllers: [EmailController],
   providers: [EmailService],
 })
--- apps/email/src/main.ts
+++ apps/email/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { EmailModule } from './email.module';
+import { RmqService } from 'libs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(EmailModule);
-  await app.listen(3000);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice(rmqService.getOptions('TELEGRAM'));
+  await app.startAllMicroservices()
 }
 bootstrap();
