f00d74c37262b82d8fd9b7d49beacea2af1fec73
D-Toshchakov <daniiltoshchakov@gmail.com>
2023-04-25 11:54:50 +0300

    enabled cors. added dockerignore to apps

--- apps/email/src/main.ts
+++ apps/email/src/main.ts
@@ -1,16 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { EmailModule } from './email.module';
-import { RmqService } from 'libs/common/src';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(EmailModule);
-  // const rmqService = app.get<RmqService>(RmqService);
-  // app.connectMicroservice(rmqService.getOptions('TELEGRAM'));
-
   const configService = app.get(ConfigService)
   const PORT = configService.get('PORT')
+  app.enableCors();
   await app.listen(PORT)
-  // await app.startAllMicroservices()
 }
 bootstrap();
